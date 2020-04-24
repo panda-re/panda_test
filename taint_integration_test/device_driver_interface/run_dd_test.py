be1c35f58f9f98e1d1d44dce56c0458145408d74
tnballo <tnballo@users.noreply.github.com>
2020-04-24 14:59:10 -0400

    pc-bios backing error

--- taint_integration_test/device_driver_interface/run_dd_test.py
+++ taint_integration_test/device_driver_interface/run_dd_test.py
@@ -11,14 +11,15 @@ from panda import blocking, Panda
 TEST_PROG_USR = "tt_ioctl_userspace"
 TEST_PROG_MOD = "tt_ioctl_module.ko"
 TEST_PROG_DIR = "dd_src"
-HOST_PROG_DIR = os.path.join(os.path.dirname(os.path.abspath(__file__)), TEST_PROG_DIR)
+CURR_DIR = os.path.dirname(os.path.abspath(__file__))
+HOST_PROG_DIR = os.path.join(CURR_DIR, TEST_PROG_DIR)
 GUEST_PROG_DIR = os.path.join(os.sep + "root", TEST_PROG_DIR)
 
 # Since the kernel module is compiled on the host, guest kernel needs to be a reasonably close version
 # Otherwise, will get "Error: could not insert module tt_ioctl_module.ko: Invalid module format"
 COMPAT_QCOW = "ubuntu_1604_x86_64.qcow"
 COMPAT_QCOW_URL = "http://panda-re.mit.edu/qcows/linux/ubuntu/1604/x86_64/" + COMPAT_QCOW
-HOST_QCOW_PATH = os.path.join(HOST_PROG_DIR, COMPAT_QCOW)
+HOST_QCOW_PATH = os.path.join(CURR_DIR, COMPAT_QCOW)
 
 def host_download_qcow():
     if not os.path.isfile(HOST_QCOW_PATH):
@@ -38,7 +39,7 @@ def host_build_test_progs():
     assert(os.path.isfile(os.path.join(HOST_PROG_DIR, TEST_PROG_MOD)))
 
 @blocking
-def run_in_guest(panda):
+def run_in_guest():
 
     # Mount test src/binaries in guest
     panda.revert_sync("root")
@@ -64,8 +65,10 @@ if __name__ == "__main__":
     panda = Panda(
         arch = "x86_64",
         qcow = HOST_QCOW_PATH,
-        args = "-nographic"
+        extra_args = "-nographic",
+        expect_prompt = rb"root@ubuntu:.*",
+        mem = "1G"
     )
 
-    panda.queue_async(run_in_guest(panda))
+    panda.queue_async(run_in_guest)
     panda.run()
