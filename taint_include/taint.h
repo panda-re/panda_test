f9794d418a1c17adc09b67a8f75962dfc32c2e2e
Jason Martin <jnmartin@ll.mit.edu>
2020-05-06 12:52:09 -0400

    added support for building taint unit tests for i386 or x86_64, conditionally

--- taint_include/taint.h
+++ taint_include/taint.h
@@ -5,149 +5,90 @@
 #include <stdint.h>
 #include <string.h>
 
-#define TARGET_I386
-
-#if !defined(TARGET_I386)
-#error "Define your architecture (TARGET_I386) with -D"
+#if !defined(TARGET_I386) && !defined(TARGET_X86_64)
+#error "Define your architecture (TARGET_I386 or TARGET_X86_64) with -D"
 #endif
 
+static const int ENABLE_TAINT = 6;
 static const int LABEL_BUFFER = 7;
 static const int LABEL_BUFFER_POS = 8;
 static const int QUERY_BUFFER = 9;
+static const int LABEL_REGISTER = 10;
+static const int QUERY_REGISTER = 11;
+static const int LOG = 12;
 
-static inline void hypercall_log(char *c_str) {
-  int eax = 12;
-  unsigned long ebx = strlen(c_str)+1;
-  void *ecx = (void *)c_str;
-  unsigned long edx = 0;
-  long edi = 0;
-
-  asm __volatile__
-      ("mov  %0, %%eax \t\n\
-        mov  %1, %%ebx \t\n\
-        mov  %2, %%ecx \t\n\
-        mov  %3, %%edx \t\n\
-        mov  %4, %%edi \t\n\
-        cpuid \t\n\
-       "
-      : /* no output registers */
-      : "g" (eax), "g" (ebx), "g" (ecx), "g" (edx), "g" (edi) /* input operands */
-       : "eax", "ebx", "ecx", "edx", "edi" /* clobbered registers */
-      );
-  return;
-}
-
-static inline void hypercall_query_reg(uint32_t reg_num, uint32_t off, long label) {
-  int eax = 11;
-  unsigned long ebx = reg_num;
-  unsigned long ecx = off;
-  unsigned long edx = 0;
-  long edi = label;
-
-  asm __volatile__
-      ("mov  %0, %%eax \t\n\
-        mov  %1, %%ebx \t\n\
-        mov  %2, %%ecx \t\n\
-        mov  %3, %%edx \t\n\
-        mov  %4, %%edi \t\n\
-        cpuid \t\n\
-       "
-      : /* no output registers */
-      : "g" (eax), "g" (ebx), "g" (ecx), "g" (edx), "g" (edi) /* input operands */
-       : "eax", "ebx", "ecx", "edx", "edi" /* clobbered registers */
-      );
-  return;
-}
-
-static inline void hypercall_label_reg(uint32_t reg_num, uint32_t off, long label) {
-  int eax = 10;
-  unsigned long ebx = reg_num;
-  unsigned long ecx = off;
-  unsigned long edx = 0;
-  long edi = label;
-
-
-  asm __volatile__
-      ("mov  %0, %%eax \t\n\
-        mov  %1, %%ebx \t\n\
-        mov  %2, %%ecx \t\n\
-        mov  %3, %%edx \t\n\
-        mov  %4, %%edi \t\n\
-        cpuid \t\n\
-       "
-      : /* no output registers */
-      : "g" (eax), "g" (ebx), "g" (ecx), "g" (edx), "g" (edi) /* input operands */
-       : "eax", "ebx", "ecx", "edx", "edi" /* clobbered registers */
-      );
-  return;
-}
-
-static inline void hypercall_enable_taint() {
-    int eax = 6;
-    int ebx,ecx,edx,edi;
-    edi = edx = ecx = ebx = eax;
-
-  asm __volatile__
-      ("mov  %0, %%eax \t\n\
-        mov  %1, %%ebx \t\n\
-        mov  %2, %%ecx \t\n\
-        mov  %3, %%edx \t\n\
-        mov  %4, %%edi \t\n\
-        cpuid \t\n\
-       "
-      : /* no output registers */
-      : "g" (eax), "g" (ebx), "g" (ecx), "g" (edx), "g" (edi) /* input operands */
-       : "eax", "ebx", "ecx", "edx", "edi" /* clobbered registers */
-      );
-  return;
-}
-
-//#ifdef TARGET_I386
+#if defined(TARGET_I386)
 static inline
-void hypercall(void *buf, unsigned long len, long label, unsigned long off,
-    int action) {
-  int eax = action;
-  void *ebx = buf;
-  unsigned long ecx = len;
-  unsigned long edx = off;
-  long edi = label;
-//  void *esi = pmli;
-
-  asm __volatile__
-      ("mov  %0, %%eax \t\n\
+void hypercall(uint32_t eax, uint32_t ebx, uint32_t ecx, uint32_t edx, uint32_t edi) {
+    asm __volatile__(
+        "mov  %0, %%eax \t\n\
         mov  %1, %%ebx \t\n\
         mov  %2, %%ecx \t\n\
         mov  %3, %%edx \t\n\
         mov  %4, %%edi \t\n\
         cpuid \t\n\
-       "
-      : /* no output registers */
-      : "g" (eax), "g" (ebx), "g" (ecx), "g" (edx), "g" (edi) /* input operands */
-       : "eax", "ebx", "ecx", "edx", "edi" /* clobbered registers */
-      );
-  return;
+        "
+        : /* no output registers */
+        : "g" (eax), "g" (ebx), "g" (ecx), "g" (edx), "g" (edi) /* input operands */
+        : "eax", "ebx", "ecx", "edx", "edi" /* clobbered registers */
+    );
+}
+
+#define HYPERCALL(a,b,c,d,e) hypercall((uint32_t)(a),(uint32_t)(b),(uint32_t)(c),(uint32_t)(d),(uint32_t)(e))
+#endif
+
+#if defined(TARGET_X86_64)
+static inline
+void hypercall(uint64_t rax, uint64_t rbx, uint64_t rcx, uint64_t rdx, uint64_t rdi) {
+    asm __volatile__(
+        "mov  %0, %%rax \t\n\
+        mov  %1, %%rbx \t\n\
+        mov  %2, %%rcx \t\n\
+        mov  %3, %%rdx \t\n\
+        mov  %4, %%rdi \t\n\
+        cpuid \t\n\
+        "
+        : /* no output registers */
+        : "g" (rax), "g" (rbx), "g" (rcx), "g" (rdx), "g" (rdi) /* input operands */
+        : "rax", "rbx", "rcx", "rdx", "rdi" /* clobbered registers */
+    );
+}
+
+#define HYPERCALL(a,b,c,d,e) hypercall((uint64_t)(a),(uint64_t)(b),(uint64_t)(c),(uint64_t)(d),(uint64_t)(e))
+#endif
+
+#if !defined(HYPERCALL)
+#error "HYPERCALL wrapper macro not defined?"
+#endif
+
+static inline
+void hypercall_log(char *c_str) {
+    HYPERCALL(LOG, strlen(c_str)+1, (void *)c_str, 0, 0);
 }
 
 static inline
-void hypercall_for_query(void *buf, unsigned long off, long label, long positive) {
-  int eax = QUERY_BUFFER;
-  void *ebx = buf;
-  unsigned long ecx = off;
-  long edx = positive;
-  long edi = label;
+void hypercall_query_reg(uint32_t reg_num, uint32_t off, long label) {
+    HYPERCALL(QUERY_REGISTER, reg_num, off, 0, label);
+}
 
-  asm __volatile__
-      ("mov  %0, %%eax \t\n\
-        mov  %1, %%ebx \t\n\
-        mov  %2, %%ecx \t\n\
-        mov  %3, %%edx \t\n\
-	mov  %4, %%edi \t\n\
-        cpuid \t\n\
-      "
-      :
-      : "g" (eax), "g" (ebx), "g" (ecx), "g" (edx), "g" (edi)
-       : "eax", "ebx", "ecx", "edx", "edi" 
-      );
+static inline
+void hypercall_label_reg(uint32_t reg_num, uint32_t off, long label) {
+    HYPERCALL(LABEL_REGISTER, reg_num, off, 0, label);
+}
+
+static inline
+void hypercall_enable_taint() {
+    HYPERCALL(ENABLE_TAINT, 0, 0, 0, 0);
+}
+
+static inline
+void hypercall_label_buffer(void *buf, unsigned long len, long label) {
+    HYPERCALL(LABEL_BUFFER, buf, len, 0, label);
+}
+
+static inline
+void hypercall_query_buffer(void *buf, unsigned long off, long label, long positive) {
+    HYPERCALL(QUERY_BUFFER, buf, off, positive, label);
 }
 
 /* buf is the address of the buffer to be labeled
@@ -155,38 +96,43 @@ void hypercall_for_query(void *buf, unsigned long off, long label, long positive
  *   * len is the length of the buffer to be labeled */
 static inline
 void panda_taint_label_buffer(void *buf, int label, unsigned long len) {
-    hypercall(buf, len, label, 0, LABEL_BUFFER);
+    hypercall_label_buffer(buf, len, label);
 }
 
 static inline
 void panda_taint_query_buffer(void *buf, unsigned long off, long label, long positive) {
-    hypercall_for_query(buf, off, label, positive);
+    hypercall_query_buffer(buf, off, label, positive);
 }
 
-static void panda_taint_assert_label_found(void *buf, uint32_t off, uint32_t expected_label) {
-        panda_taint_query_buffer(buf, off, expected_label, 1);
+static inline
+void panda_taint_assert_label_found(void *buf, uint32_t off, uint32_t expected_label) {
+    panda_taint_query_buffer(buf, off, expected_label, 1);
 }
 
-static void panda_taint_assert_label_found_range(void *buf, size_t len, uint32_t expected_label) {
+static inline
+void panda_taint_assert_label_not_found(void *buf, uint32_t off, uint32_t expected_label) {
+    panda_taint_query_buffer(buf, off, expected_label, 0);
+}
+
+static inline
+void panda_taint_assert_label_found_range(void *buf, size_t len, uint32_t expected_label) {
     int i;
     for(i=0;i<len;i++) {
         panda_taint_assert_label_found(buf, i, expected_label);
     }
 }
 
-static void panda_taint_assert_label_not_found(void *buf, uint32_t off, uint32_t expected_label) {
-        panda_taint_query_buffer(buf, off, expected_label, 0);
-}
-
-static void panda_taint_assert_label_not_found_range(void *buf, size_t len, uint32_t expected_label) {
+static inline
+void panda_taint_assert_label_not_found_range(void *buf, size_t len, uint32_t expected_label) {
     int i;
     for(i=0;i<len;i++) {
         panda_taint_assert_label_not_found(buf, i, expected_label);
     }
 }
 
-static void panda_taint_log(char *c_str) {
+static inline
+void panda_taint_log(char *c_str) {
     hypercall_log(c_str);
 }
 
-#endif
+#endif // __TAINT_H__
--- taint_unit_test/src/eax_test.asm
+++ taint_unit_test/src/eax_test.asm
@@ -1,12 +1,4 @@
-; EXPECTED OUTPUT
-; taint2: positional register label
-; taint2: query labels on memory
-; taint2: assert positive taint_contains(addr: BFC0E5BC, label: 12345678)
-; taint2: query labels on memory
-; taint2: assert negative taint_contains(addr: BFC0E5B4, label: 12345678)
-; taint2: query labels on memory
-; taint2: assert positive taint_contains(addr: BFC0E5BC, label: 12345678)
-
+%ifdef TARGET_I386
 	global	_start
 	section .text
 
@@ -132,3 +124,133 @@ _start:
 
 	section	.bss
 test_word:	resw	1
+
+%endif
+
+%ifdef TARGET_X86_64
+	global	_start
+	section .text
+
+_start:
+; set taint on EAX (register 0)
+	mov	rax, 10
+	mov	rbx, 0
+	mov	rcx, 0
+	mov	rdx, 0
+	mov	rdi, 0x12345678
+	cpuid
+
+; if taint system works correctly and EAX is tainted, we can check for this
+; by pushing eax to stack, then testing the stack for taint label
+; this time, it should be positive for taint
+        push    rax
+        mov     rax, 9
+        mov     rbx, rsp
+        mov     rcx, 0
+        mov     rdx, 0
+        mov     rdi, 0x12345678
+        cpuid
+        pop     rax
+
+; remove taint from the stack memory
+	push	0
+; test negative
+	push	rax
+        mov     rax, 9
+	; testing stack-4 , the previous frame that we wiped with the push 0
+        mov     rbx, rsp
+	sub	rbx, 4
+        mov     rcx, 0
+        mov     rdx, 0
+        mov     rdi, 0x12345678
+        cpuid
+	pop	rax
+	add	rsp, 4
+
+; repeat the test to see if "pop eax" preserves taint by pushing eax back to stack and testing it again
+; stack should still test positive for taint which was transferred from eax if so
+	push	rax
+	mov	rax, 9
+	mov	rbx, rsp
+	mov	rcx, 0
+	mov	rdx, 0
+	mov	rdi, 0x12345678
+	cpuid
+	pop	rax
+
+; addition by constant, still tainted
+	add	rax, 1
+	push	rax
+        mov     rax, 9
+        mov     rbx, rsp
+        mov     rcx, 0
+        mov     rdx, 0
+        mov     rdi, 0x12345678
+        cpuid
+        pop     rax
+
+; subtraction, still tainted
+        sub     rax, 1
+        push    rax
+        mov     rax, 9
+        mov     rbx, rsp
+        mov     rcx, 0
+        mov     rdx, 0
+        mov     rdi, 0x12345678
+        cpuid
+        pop     rax
+
+; mult, still
+	imul	rax, 1024
+        push    rdx
+	push	rax
+
+        mov     rax, 9
+        mov     rbx, rsp
+	sub	rbx, 4
+        mov     rcx, 0
+        mov     rdx, 0
+        mov     rdi, 0x12345678
+        cpuid
+
+	mov	rax, 9
+	mov	rbx, rsp
+	mov	rcx, 0
+	mov	rdx, 0
+	mov	rdi, 0x12345678
+	cpuid
+
+	pop	rax
+        pop     rdx
+
+; div, still
+        div     rax
+        push    rdx
+	push	rax
+
+        mov     rax, 9
+        mov     rbx, rsp
+        sub     rbx, 4
+        mov     rcx, 0
+        mov     rdx, 0
+        mov     rdi, 0x12345678
+        cpuid
+
+        mov     rax, 9
+        mov     rbx, rsp
+        mov     rcx, 0
+        mov     rdx, 0
+        mov     rdi, 0x12345678
+        cpuid
+
+	pop	rax
+        pop     rdx
+
+	mov	rax, 60
+	xor	rdi, rdi
+	syscall
+
+	section	.bss
+test_word:	resw	1
+
+%endif
